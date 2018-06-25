c37e93338f44e84ee74a5a674caa51a20495b524
callan <callan@tuta.io>
2018-06-25 14:33:06 +0930

    basic interface and data input

--- log.py
+++ log.py
@@ -7,8 +7,23 @@ class User:
         self.id = id
         self.name = name
         self.goal = goal
+        self.data = 'data/' + str(self.id) + '.csv'
 
-    def log_data(self, weight):
-        with open('data/' + id + '.csv', 'a') as data:
+    def log_data(self, weight, pcent):
+        with open(self.data, 'a') as data:
             write = csv.writer(data)
-            date = time.strftime('%d/%m/%Y')
+            date = time.strftime('%Y%m%d')
+            write.writerow([date, self.id, weight, pcent])
+
+
+if __name__ == '__main__':
+    print('hi, you know what to do, so do the thing that you are supposed to')
+    name = input('name?\n')
+    goal = input('goal?\n')
+    weight = input('weight?\n')
+    pcent = input('percentage? (optional)\n')
+    if name == 'jo' or name == 'Jo':
+        user = User(1, name, goal)
+    elif name == 'Callan' or name == 'callan':
+        user = User(2, name, goal)
+    user.log_data(weight, pcent)
--- log_testing.py
+++ log_testing.py
@@ -1,15 +1,38 @@
-import log
 import unittest
+from pathlib import Path
+import time
+#import os
+
+import log
 
 
 class UserTest(unittest.TestCase):
     def setUp(self):
         self.u = log.User(1, 'test', 100)
 
-    def test_u_attributes(self):
+    def test_user_attributes(self):
         self.assertEqual(self.u.id, 1)
         self.assertEqual(self.u.name, 'test')
         self.assertEqual(self.u.goal, 100)
+
+    def test_check_file_created(self):
+        self.u.log_data(100, 93.2)
+        path = 'data/' + str(self.u.id) + '.csv'
+        path = Path(path)
+        self.assertTrue(path.is_file())
+
+    def test_check_data_written(self):
+        self.u.log_data(15, 18.5)
+        path = 'data/' + str(self.u.id) + '.csv'
+        today = time.strftime('%Y%m%d')
+        with open(path, 'rb') as f:
+            f.seek(-2, 2)
+            while f.read(1) != b'\n':  #why is b here?
+                f.seek(-2, 1)
+            last = f.readline()
+            line = today.encode('utf-8') + b',' + str(
+                self.u.id).encode('utf-8') + b',15,18.5\r\r\n'
+            self.assertEqual(last, line)
 
 
 if __name__ == '__main__':
